2043eda1a3c6e126e3b089b6fd2fba83f97177fd
graxel <grazel98@gmail.com>
2026-04-20 10:51:48 -0400

    update dbt staging layer

--- data_pipeline/dbt/models/staging/stg_station_information.sql
+++ data_pipeline/dbt/models/staging/stg_station_information.sql
@@ -4,5 +4,7 @@ SELECT
     (raw_json->>'lat')::float as lat,
     (raw_json->>'lon')::float as lon,
     (raw_json->>'capacity')::int as capacity,
+    (raw_json->>'has_kiosk')::boolean as has_kiosk,
+    (raw_json->>'region_id')::varchar as region_id,
     ingested_at
 FROM {{ source('raw', 'station_information') }}
--- data_pipeline/dbt/models/staging/stg_station_status.sql
+++ data_pipeline/dbt/models/staging/stg_station_status.sql
@@ -1,12 +1,13 @@
 SELECT
     (raw_json->>'station_id')::varchar as station_id,
-    (raw_json->>'num_bikes_available')::int as num_bikes_available,
+    to_timestamp((raw_json->>'last_reported')::numeric) as last_reported,
+    ingested_at,
     (raw_json->>'num_docks_available')::int as num_docks_available,
-    (raw_json->>'num_bikes_disabled')::int as num_bikes_disabled,
     (raw_json->>'num_docks_disabled')::int as num_docks_disabled,
+    (raw_json->>'num_bikes_available')::int as num_bikes_available,
+    (raw_json->>'num_ebikes_available')::int as num_ebikes_available,
+    (raw_json->>'num_bikes_disabled')::int as num_bikes_disabled,
     (raw_json->>'is_installed')::boolean as is_installed,
     (raw_json->>'is_renting')::boolean as is_renting,
-    (raw_json->>'is_returning')::boolean as is_returning,
-    to_timestamp((raw_json->>'last_reported')::numeric) as last_reported,
-    ingested_at
+    (raw_json->>'is_returning')::boolean as is_returning
 FROM {{ source('raw', 'station_status') }}
